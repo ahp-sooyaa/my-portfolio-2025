fda860fe241a777a8a6cf33a6dde57f08ca07e4d
Aung Htet Paing <aunghtet@clickrmedia.com>
2025-03-28 15:07:06 +0700

    initial project setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,18 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-sanity',
     options: {
-      "projectId": "vbf9huw5",
-      "dataset": ""
+      "projectId": process.env.SANITY_PROJECT_ID,
+      "dataset": process.env.SANITY_DATASET,
+      "token": process.env.SANITY_TOKEN,
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", {
+    resolve: 'gatsby-plugin-google-gtag',
+    options: {
+      trackingIds: [
+        'G-FV09K3D603',
+      ],
+    },
+  }, "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
